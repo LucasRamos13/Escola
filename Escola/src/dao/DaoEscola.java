1f3f96dae0754ef3f6bcb1dba6ba2a475a5ec341
Administrador <Administrador@10.10.4.12>
2019-08-08 15:46:07 -0300

    Jorge

---
+++ Escola/src/dao/DaoEscola.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import modelo.Escola;
+/**
+ *
+ * @author Administrador
+ */
+public class DaoEscola {
+     public static boolean inserir(Escola objeto) {
+        String sql = "INSERT INTO Escola (sigla, nome, endereco) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getSigla());
+            ps.setString(2, objeto.getNome());
+            ps.setString(3, objeto.getEndereco());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+      public static void main(String[] args) {
+        Escola objeto = new Escola();
+        objeto.setSigla("IFRS");
+        objeto.setNome("Jorge Pereira");
+        objeto.setEndereco("Jooj da Silva Rua bastos");
+        
+        boolean resultado = inserir(objeto);
+        if (resultado) {
+            JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
+        } else {
+            JOptionPane.showMessageDialog(null, "Erro!");
+        }
+    }
+        public static boolean alterar(Escola objeto) {
+        String sql = "UPDATE Escola SET nome = ?, endereco = ?, sigla = ? WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setString(1, objeto.getNome()); 
+            ps.setString(2, objeto.getEndereco());
+            ps.setString(3, objeto.getSigla());
+            ps.setInt(4, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+          public static boolean excluir(Escola objeto) {
+        String sql = "DELETE FROM Escola WHERE codigo=?";
+        try {
+            PreparedStatement ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ps.setInt(1, objeto.getCodigo());
+            ps.executeUpdate();
+            return true;
+        } catch (SQLException | ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+            return false;
+        }
+    }
+}
---
+++ Escola/src/modelo/Escola.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Escola {
+  private Integer codigo;
+  private String Nome;
+  private String Endereco;
+  private String Sigla;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public String getEndereco() {
+        return Endereco;
+    }
+
+    public void setEndereco(String Endereco) {
+        this.Endereco = Endereco;
+    }
+
+    public String getSigla() {
+        return Sigla;
+    }
+
+    public void setSigla(String Sigla) {
+        this.Sigla = Sigla;
+    }
+
+    @Override
+    public String toString() {
+        return "Escola{" + "codigo=" + codigo + ", Nome=" + Nome + ", Endereco=" + Endereco + ", Sigla=" + Sigla + '}';
+    }
+  
+}
